54a799d911a48be8069fbc42a62a166c186bd3de
AndrewAscar742 <andrewmatosascar@outlook.com>
2023-03-13 15:02:17 -0300

    Feature: Adding Routes and NavComponent Created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+//Aqui serão colocados componentes em que pode colocar as rotas
+const routes: Routes = [
+  {
+    path: "", component: NavComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
